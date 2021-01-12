810c0aafe83437f4b43f2a51140624a1d4c31377
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-01-12 17:31:07 +0900

    yaml

---
+++ lib/models/ModelProvider.dart
@@ -0,0 +1,37 @@
+/*
+* Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
+import 'Sessions.dart';
+import 'Trainers.dart';
+import 'Users.dart';
+
+export 'Sessions.dart';
+export 'Trainers.dart';
+export 'Users.dart';
+
+class ModelProvider implements ModelProviderInterface {
+  @override
+  String version = "2a00f0daf860ab694042b67aa4bd5385";
+  @override
+  List<ModelSchema> modelSchemas = [
+    Sessions.schema,
+    Trainers.schema,
+    Users.schema
+  ];
+  static final ModelProvider _instance = ModelProvider();
+
+  static ModelProvider get instance => _instance;
+}
