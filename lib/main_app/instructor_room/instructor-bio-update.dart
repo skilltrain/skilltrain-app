b72c0f488fd934b65cdf33e4dd344d522411a99d
dmacdermott <damianmacdermott@gmail.com>
2021-01-12 18:34:12 +0900

    added packages for image upload and button on frontend

--- lib/main_app/instructor_room/instructor-bio-update.dart
+++ lib/main_app/instructor_room/instructor-bio-update.dart
@@ -1,9 +1,67 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
-//import './session.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+import './../../amplifyconfiguration.dart';
 
-class InstructorBioUpdate extends StatelessWidget {
-  final int index;
-  InstructorBioUpdate({this.index});
+class InstructorBioUpdate extends StatefulWidget {
+  @override
+  _InstructorBioUpdateState createState() => _InstructorBioUpdateState();
+}
+
+class _InstructorBioUpdateState extends State<InstructorBioUpdate> {
+  int index;
+
+  bool _isAmplifyConfigured = false;
+  String _uploadFileResult = '';
+  String _getUrlResult = '';
+  String _removeResult = '';
+  Amplify amplify = new Amplify();
+
+  @override
+  void initState() {
+    super.initState();
+
+    configureAmplify();
+  }
+
+  void configureAmplify() async {
+    // First add plugins (Amplify native requirements)
+    AmplifyStorageS3 storage = new AmplifyStorageS3();
+    AmplifyAuthCognito auth = new AmplifyAuthCognito();
+    amplify.addPlugin(authPlugins: [auth], storagePlugins: [storage]);
+
+    // Configure
+    await amplify.configure(amplifyconfig);
+
+    setState(() {
+      _isAmplifyConfigured = true;
+    });
+  }
+
+  void _upload() async {
+    try {
+      File local = await FilePicker.getFile(type: FileType.image);
+      final key = 'ExampleKey';
+      Map<String, String> metadata = <String, String>{};
+      metadata['type'] = 'classPhoto';
+      S3UploadFileOptions options = S3UploadFileOptions(
+          accessLevel: StorageAccessLevel.guest, metadata: metadata);
+      UploadFileResult result = await Amplify.Storage.uploadFile(
+          key: key, local: local, options: options);
+      setState(() {
+        _uploadFileResult = result.key;
+      });
+    } catch (e) {
+      print('UploadFile Err: ' + e.toString());
+    }
+  }
+
+  void getUrl() async {}
+
+  void _download() async {}
 
   @override
   Widget build(BuildContext context) {
@@ -11,118 +69,117 @@ class InstructorBioUpdate extends StatelessWidget {
         appBar: AppBar(
           title: Text("bio update"),
         ),
-        body:     
-          SingleChildScrollView(
-            child:Container(
-              child:Column(
-              children:<Widget>[
-              Container(
-                  padding: const EdgeInsets.only(top:8.0, left:8.0, right:8.0 , bottom: 10.0),
-
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: 300,
-                  padding: const EdgeInsets.all(4.0,),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black54, width:3),
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.white70,
-                  ),
-                  child: Column(
-                      children: <Widget>[
-                      Image.asset('assets/images/bio.png',
-                      height: 150, fit: BoxFit.fill),
-                      Text("Upload portrait",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.black54),
-                      )                      
-                          ],
-                  ),
+        body: SingleChildScrollView(
+            child: Container(
+          child: Column(children: <Widget>[
+            Container(
+                padding: const EdgeInsets.all(
+                  10.0,
                 ),
-
-                Container(
-                  width: 300,
-                  padding: const EdgeInsets.all(4.0,),
-                  margin: const  EdgeInsets.only(top:6.0,),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black54, width:3),
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.white70,
-                  ),
-                  child: Column(
-                      children: <Widget>[
-                      Image.asset('assets/images/bio.png',
-                      height: 150, fit: BoxFit.fill),
-                      Text("Upload classPhoto",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.black54),
-                      )                      
-                          ],
-                  ),
-                ),                
-
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'genre'),
-                ),
-
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'price'),
-                ),
-
-                TextFormField(
-                          decoration: InputDecoration(labelText: 'bio',),
-                          maxLines: 4,
-                          minLines: 4,
-                  ),
-
-                  ]
-                )
-              ),
-
-                RaisedButton(
-                  onPressed: () {
-                  },
-                  textColor: Colors.white,
-                  padding: const EdgeInsets.all(0),
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: <Color>[
-                          Colors.pink[300],
-                          Colors.purple[500],
-                          Colors.purple[700],
-                        ],
-                      ),
+                child: Column(children: <Widget>[
+                  Container(
+                    width: 300,
+                    padding: const EdgeInsets.all(
+                      5.0,
                     ),
-                    child: 
-                    Container(
-                      padding: EdgeInsets.all(5.0),
-                      child: Text('Uodate profile',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black54, width: 3),
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white70,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RaisedButton(
+                          onPressed: _upload,
+                          child: const Text('Upload File'),
+                        ),
+                      ],
+                    ),
+                    // child: Column(
+                    //   children: <Widget>[
+                    //     Image.asset('assets/images/bio.png',
+                    //         height: 150, fit: BoxFit.fill),
+                    //     Text(
+                    //       "Upload portrait",
+                    //       textAlign: TextAlign.left,
+                    //       style: TextStyle(
+                    //           fontWeight: FontWeight.bold,
+                    //           fontSize: 20,
+                    //           color: Colors.black54),
+                    //     )
+                    //   ],
+                    // ),
+                  ),
+                  Container(
+                    width: 300,
+                    padding: const EdgeInsets.all(
+                      5.0,
+                    ),
+                    margin: const EdgeInsets.only(
+                      top: 5.0,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black54, width: 3),
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white70,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('assets/images/bio.png',
+                            height: 150, fit: BoxFit.fill),
+                        Text(
+                          "Upload classPhoto",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              )),
-                    )                    
+                              fontSize: 20,
+                              color: Colors.black54),
+                        )
+                      ],
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'genre'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'price'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'bio',
+                    ),
+                    maxLines: 4,
+                    minLines: 4,
+                  ),
+                ])),
+            RaisedButton(
+              onPressed: () {},
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Colors.pink[300],
+                      Colors.purple[500],
+                      Colors.purple[700],
+                    ],
                   ),
                 ),
-
-        ]
-        ),
-        )
-      )
-    );
+                child: const Text('Uodate profile',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    )),
+              ),
+            ),
+          ]),
+        )));
   }
 }
-
 
 class ChangeForm extends StatefulWidget {
   @override
@@ -130,7 +187,6 @@ class ChangeForm extends StatefulWidget {
 }
 
 class _ChangeFormState extends State<ChangeForm> {
-
   String _text = '';
 
   void _handleText(String e) {
@@ -149,23 +205,20 @@ class _ChangeFormState extends State<ChangeForm> {
               style: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 30.0,
-                  fontWeight: FontWeight.w500
-              ),
+                  fontWeight: FontWeight.w500),
             ),
             new TextField(
               enabled: true,
               // 入力数
               maxLength: 10,
-              // ignore: deprecated_member_use
               maxLengthEnforced: false,
               style: TextStyle(color: Colors.red),
               obscureText: false,
-              maxLines:1 ,
+              maxLines: 1,
               //パスワード
               onChanged: _handleText,
             ),
           ],
-        )
-    );
+        ));
   }
 }
