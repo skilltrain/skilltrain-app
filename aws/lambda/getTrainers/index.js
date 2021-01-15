8771fcefc6811e8ac6304fa71ae1075746611cef
dmacdermott <damianmacdermott@gmail.com>
2021-01-15 15:40:27 +0900

    updated getTrainers lambda to allow for queries

--- aws/lambda/getTrainers/index.js
+++ aws/lambda/getTrainers/index.js
@@ -5,19 +5,42 @@ AWS.config.update({ region: "ap-northeast-1" });
 exports.handler = async function (event, context) {
   const ddb = new AWS.DynamoDB.DocumentClient();
 
-  const params = {
-    TableName: "Trainers",
-  };
+  const queries = event.queryStringParameters;
 
   let responseBody;
   let statusCode;
+
+  const getQueryExpression = fields => {
+    let exp = {
+      FilterExpression: "",
+      ExpressionAttributeNames: {},
+      ExpressionAttributeValues: {},
+    };
+    Object.entries(fields).forEach(([key, item]) => {
+      exp.FilterExpression += ` #${key} = :${key} AND`;
+      exp.ExpressionAttributeNames[`#${key}`] = key;
+      exp.ExpressionAttributeValues[`:${key}`] = item;
+    });
+    console.log(exp.FilterExpression);
+    exp.FilterExpression = exp.FilterExpression.slice(0, -4);
+    console.log(exp.FilterExpression);
+
+    return exp;
+  };
+
+  let expression = getQueryExpression(queries) || null;
+
+  const params = {
+    TableName: "Trainers",
+    ...expression,
+  };
 
   try {
     const data = await ddb.scan(params).promise();
     responseBody = JSON.stringify(data.Items);
     statusCode = 200;
   } catch (error) {
-    responseBody = `Unable to get trainers: ${error}`;
+    responseBody = `Unable to get Trainers: ${error}`;
     statusCode = 403;
   }
 
