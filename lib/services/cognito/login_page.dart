0dd5a2c9ec90ba0029711b375597f47156e87c74
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-01-17 16:49:32 +0900

    Delete uneccessary code

--- lib/services/cognito/login_page.dart
+++ lib/services/cognito/login_page.dart
@@ -60,30 +60,6 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  // void showAlertDialog(BuildContext context, String errorMessage) {
-  //   Widget okButton = FlatButton(
-  //     child: Text("OK"),
-  //     onPressed: () {
-  //       Navigator.of(context, rootNavigator: true).pop();
-  //     },
-  //   );
-
-  //   AlertDialog alert = AlertDialog(
-  //     title: Text("Error"),
-  //     content: Text(errorMessage),
-  //     actions: [
-  //       okButton,
-  //     ],
-  //   );
-
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return alert;
-  //     },
-  //   );
-  // }
-
   void _login() async {
     final username = _usernameController.text.trim();
     final password = _passwordController.text.trim();
