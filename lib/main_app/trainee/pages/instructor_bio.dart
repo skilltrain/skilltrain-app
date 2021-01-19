2415261c6d5fc5da3fd9835259b0a48644c92322
dmacdermott <damianmacdermott@gmail.com>
2021-01-19 19:55:21 +0900

    added first last name properties instead of instructor also added rating

--- lib/main_app/trainee/pages/instructor_bio.dart
+++ lib/main_app/trainee/pages/instructor_bio.dart
@@ -9,66 +9,76 @@ class InstructorBio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> stars = [];
+    for (var i = 0; i < data["avgRating"]; i++) {
+      stars.add(Icon(Icons.star, color: Colors.yellow[700], size: 14));
+    }
     return Scaffold(
-      appBar: AppBar(
-        title: Text("instructor bios"),
-      ),
-      body: 
-      SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Image.network(data["profilePhoto"]),
-          Row(children: <Widget>[
-            Text(data["instructor"],
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                )),
-          ]),
-          Text(
-            data["bio"],
-            style: TextStyle(
-              fontSize: 15,
-            ),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                SlideLeftRoute(
-                    page: BookingPage(
-                  price: data["price"],
-                  trainerName: data["username"],
-                  index: index,
-                )),
-              );
-            },
-            textColor: Colors.white,
-            padding: const EdgeInsets.all(0),
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Colors.pink[300],
-                    Colors.purple[500],
-                    Colors.purple[700],
-                  ],
+        appBar: AppBar(
+          title: Text(data["firstName"] + " " + data["lastName"]),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image.network(data["profilePhoto"]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: <Widget>[
+                  Text(data["firstName"] + " " + data["lastName"],
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      )),
+                  Spacer(),
+                  Row(children: stars),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  data["bio"],
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
                 ),
               ),
-              padding: const EdgeInsets.all(10),
-              child: const Text('check availability now! >>>',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  )),
-            ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    SlideLeftRoute(
+                        page: BookingPage(
+                      price: data["price"],
+                      trainerName: data["username"],
+                      index: index,
+                    )),
+                  );
+                },
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Colors.pink[300],
+                        Colors.purple[500],
+                        Colors.purple[700],
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: const Text('check availability now! >>>',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      )),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      )
-    );
+        ));
   }
 }
