ad7037cb65f65d208bd27cf022291a59793a4d03
Yuta <ipa.freaks@gmail.com>
2021-01-21 15:48:09 +0900

    quick fix / import files missing issue  was fixed

--- lib/main_app/trainee/home_page_trainee.dart
+++ lib/main_app/trainee/home_page_trainee.dart
@@ -13,6 +13,7 @@ import '../common/fetchTrainers.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import '../common/headings.dart';
 import '../trainee/pages/trainee_session_detail.dart';
+import '../trainer/pages/instructor_view/pages/instructor_session_detail.dart';
 
 class HomePageTrainee extends StatefulWidget {
   final VoidCallback shouldLogOut;
