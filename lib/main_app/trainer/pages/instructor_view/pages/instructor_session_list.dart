d6f3bbb37e100f66619e6a3bed0faa57a44f1323
dmacdermott <damianmacdermott@gmail.com>
2021-01-24 20:06:57 +0900

    trainer nearly done, few bugs around

--- lib/main_app/trainer/pages/instructor_view/pages/instructor_session_list.dart
+++ lib/main_app/trainer/pages/instructor_view/pages/instructor_session_list.dart
@@ -42,25 +42,6 @@ class SampleStart extends State<SessionList> {
   String stringDate = format.format(new DateTime.now()
       .subtract(Duration(days: 1))); //default date value for card
 
-  // Future<Null> _selectDate(BuildContext context) async {
-  //   final DateTime picked = await showDatePicker(
-  //       context: context,
-  //       initialDate: _date,
-  //       firstDate: new DateTime(2016),
-  //       lastDate: new DateTime.now().add(new Duration(days: 360)));
-
-  //   //Date format into String
-
-  //   if (picked != null)
-  //     setState(() => {
-  //           _date = picked,
-  //           stringDate = format.format(_date),
-  //           print(_date),
-  //           print(stringDate)
-  //         });
-  // }
-//calendar object
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -167,7 +148,6 @@ class SampleStart extends State<SessionList> {
                                                                 ['id'])),
                                               )
                                             });
-//
                                   },
                                   itemCount: classArray.length,
                                 )),
@@ -196,170 +176,7 @@ class SampleStart extends State<SessionList> {
                 ),
               ),
             ],
-          )
-
-//         body: Column(
-//           children: <Widget>[
-//             FutureBuilder<List>(
-//               future: sessionResults,
-//               builder: (context, snapshot) {
-//                 if (snapshot.hasData) {
-//                   final List classArray = [];
-//                   for (int i = 0; i < snapshot.data.length; i++) {
-//                     if (snapshot.data[i]["trainer_username"] == trainerName &&
-//                         DateTime.parse(stringDate).isBefore(
-//                             DateTime.parse(snapshot.data[i]["date"]))) {
-//                       print(stringDate);
-//                       print(snapshot.data[i]["date"]);
-//                       classArray.add(snapshot.data[i]);
-//                       classArray.sort((a, b) {
-//                         var adate = a["date"] + a["start_time"];
-//                         var bdate = b["date"] + b["start_time"];
-//                         return adate.compareTo(bdate);
-//                       });
-//                     } else
-//                       print("something went wrong with fetched data");
-//                   }
-
-// //calendar object
-//                   return Container(
-//                       height: 678,
-//                       width: double.infinity,
-//                       padding: const EdgeInsets.all(50.0),
-//                       child: Column(
-//                         children: <Widget>[
-//                           Center(child: Text("last update:" + "$_date")),
-//                           // new RaisedButton(
-//                           //   onPressed: () => _selectDate(context),
-//                           //   child: new Text('日付選択'),
-//                           // ),
-//                           SizedBox(
-//                               height: 514,
-//                               child: ListView.builder(
-//                                 shrinkWrap: true,
-//                                 itemBuilder: (BuildContext context, int index) {
-//                                   return Card(
-//                                       child: GestureDetector(
-//                                           //画面遷移
-//                                           onTap: () => {
-//                                                 Navigator.push(
-//                                                     context,
-//                                                     SlideLeftRoute(
-//                                                         page: InstructorSessionDetail(
-//                                                             sessionID:
-//                                                                 classArray[
-//                                                                         index]
-//                                                                     ['id']))),
-//                                               },
-//                                           child: Column(
-//                                             children: <Widget>[
-//                                               Row(
-//                                                   mainAxisAlignment:
-//                                                       MainAxisAlignment.start,
-//                                                   crossAxisAlignment:
-//                                                       CrossAxisAlignment.center,
-//                                                   children: <Widget>[
-//                                                     Column(
-//                                                         crossAxisAlignment:
-//                                                             CrossAxisAlignment
-//                                                                 .start,
-//                                                         children: <Widget>[
-//                                                           Text(
-//                                                             classArray[index]
-//                                                                 ["date"],
-//                                                             textAlign:
-//                                                                 TextAlign.left,
-//                                                             style: TextStyle(
-//                                                               fontWeight:
-//                                                                   FontWeight
-//                                                                       .bold,
-//                                                               fontSize: 20,
-//                                                             ),
-//                                                           ),
-//                                                           Text(
-//                                                               classArray[index][
-//                                                                       "start_time"] +
-//                                                                   " - " +
-//                                                                   classArray[
-//                                                                           index]
-//                                                                       [
-//                                                                       "end_time"],
-//                                                               textAlign:
-//                                                                   TextAlign
-//                                                                       .left,
-//                                                               style: TextStyle(
-//                                                                 fontWeight:
-//                                                                     FontWeight
-//                                                                         .bold,
-//                                                                 fontSize: 16,
-//                                                               )),
-//                                                           /*
-//                                                           Text(
-//                                                             "With " +
-//                                                                 classArray[
-//                                                                         index][
-//                                                                     "user_username"],
-//                                                             textAlign:
-//                                                                 TextAlign.left,
-//                                                           ),
-//                                                           */
-//                                                         ]),
-//                                                     new Spacer(),
-//                                                     Column(
-//                                                       children: <Widget>[
-//                                                         Text(
-//                                                           "Session Code",
-//                                                           textAlign:
-//                                                               TextAlign.right,
-//                                                         ),
-//                                                         Text(
-//                                                             classArray[index]
-//                                                                 ["sessionCode"],
-//                                                             textAlign:
-//                                                                 TextAlign.right,
-//                                                             style: TextStyle(
-//                                                               fontWeight:
-//                                                                   FontWeight
-//                                                                       .bold,
-//                                                               fontSize: 15,
-//                                                             ))
-//                                                       ],
-//                                                     ),
-//                                                     new Spacer(),
-//                                                     ButtonTheme(
-//                                                       minWidth: 30,
-//                                                       child: RaisedButton(
-//                                                           child: Icon(Icons
-//                                                               .video_call_rounded),
-//                                                           onPressed: () => {
-//                                                                 Navigator.push(
-//                                                                     context,
-//                                                                     SlideLeftRoute(
-//                                                                         page:
-//                                                                             IndexPageTrainer()))
-//                                                               }),
-//                                                     ),
-//                                                   ]),
-//                                             ],
-//                                           )));
-//                                 },
-//                                 itemCount: classArray.length,
-//                               )),
-//                         ],
-//                       ));
-
-// //calendar object
-//                 } else if (snapshot.connectionState != ConnectionState.done) {
-//                   return CircularProgressIndicator();
-//                 } else if (snapshot.hasError) {
-//                   return Text("${snapshot.error}");
-//                 }
-//                 return CircularProgressIndicator();
-//               },
-//             ),
-//           ],
-//         ),
-          ),
+          )),
     );
   }
 }
